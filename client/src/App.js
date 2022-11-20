0aeb1c2ded1428998f5d300319b88e5c645a9c4b
Aleks Nikolov <tcr.official.world@gmail.com>
2022-11-20 23:51:16 +0200

    work started on header

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { HeaderComponent } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+        <HeaderComponent></HeaderComponent>
       <h1>site</h1>
     </div>
   );
