4ae7546166136bce2a2257d4faad2730b70ab46f
Aleks Nikolov <tcr.official.world@gmail.com>
2022-11-21 22:30:12 +0200

    base header structure

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,13 +3,14 @@ export const HeaderComponent = () => {
     return (
         <header>
             <section className="left">
-
+                 {/* image as a logo on the left */}
+                 <h2>Дълбоката държава</h2>
             </section>
             <section className="right">
                 <ul>
-                    <li><a href="/"></a></li>
-                    <li><a href="/"></a></li>
-                    <li><a href="/"></a></li>
+                    <li><a href="/">Начална страница</a></li>
+                    <li><a href="/">Епизоди</a></li>
+                    <li><a href="/">Гости</a></li>
                 </ul>
             </section>
         </header>
