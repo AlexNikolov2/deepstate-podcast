82ebc1ca1b81b5defb08a9a7886d6a743a04a83d
Aleks Nikolov <72394391+AlexNikolov2@users.noreply.github.com>
2022-11-24 17:24:23 +0200

    Work from 21/11 (#6)

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import image from "../../assets/img/Untitled.png"
+import './Header.css'
+
 export const HeaderComponent = () => {
     return (
         <header>
-            <h1>content</h1>
+            <section className="left">
+                 <img src={image} alt="" />
+                 <h2>Дълбоката държава</h2>
+            </section>
+            <section className="right">
+                <ul>
+                    <li><a href="/">Начална страница</a></li>
+                    <li><a href="/">Епизоди</a></li>
+                    <li><a href="/">Гости</a></li>
+                </ul>
+            </section>
         </header>
     )
 }
