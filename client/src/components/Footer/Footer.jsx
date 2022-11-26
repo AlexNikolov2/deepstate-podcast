06dade53474b39698760c22642ef18ae0f5d4c1d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-11-26 23:26:26 +0200

    started work on footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <h2>Дълбоката Държава</h2>
+                <p>2023. All rights reserved.</p>
+            </section>
+            <section>
+                <p>*links to our social networks*</p>
+            </section>
+        </footer>
+    )
+}
