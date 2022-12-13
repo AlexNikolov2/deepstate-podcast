4c68c1ad1da5f51e2d28e8e4368f78def50a51c3
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-13 13:04:34 +0200

    work on header, stylization

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,3 +1,23 @@
+import image from '../../assets/img/Untitled.png';
+
 export const HeaderComponent = () => {
-  return <header className="">Ljubavi moja</header>;
+  return (
+    <header className="flex justify-around">
+      <div id="left" className="flex">
+        <img src={image} alt="" className="w-20" />
+        <p className="text-3xl">Дълбоката държава</p>
+      </div>
+      <ul className="flex gap-3">
+        <li>
+          <a href="/">Начало</a>
+        </li>
+        <li>
+          <a href="/">Епизоди</a>
+        </li>
+        <li>
+          <a href="/">Гости</a>
+        </li>
+      </ul>
+    </header>
+  );
 };
