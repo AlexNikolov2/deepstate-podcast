922f7db38d4fcfc128968cc0c2b3728ffd7e91f2
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-13 12:40:24 +0200

    placed font styling on app

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,3 +1,3 @@
 export const HeaderComponent = () => {
-  return <header className="bg-slate-800 exo-2 text-6xl font-bold">Ljubavi moja</header>;
+  return <header className="">Ljubavi moja</header>;
 };
