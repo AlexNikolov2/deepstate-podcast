80cb3120122b425f0067868aa0e24580ec96ad92
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-13 23:17:57 +0200

    added working hamburger

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import image from '../../assets/img/Untitled.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { hamburger } from './utils/navButton';
 
 export const HeaderComponent = () => {
   return (
-    <header className="flex justify-around">
+    <header className="flex justify-around" id="header">
       <div id="left" className="flex">
         <img src={image} alt="" className="w-20" />
         <p className="text-3xl">Дълбоката държава</p>
       </div>
-      <ul className="flex gap-3">
+      <ul className="hidden md:flex gap-3" id="navbar">
         <li>
           <a href="/">Начало</a>
         </li>
@@ -18,6 +21,9 @@ export const HeaderComponent = () => {
           <a href="/">Гости</a>
         </li>
       </ul>
+      <button onClick={hamburger} className="md:hidden">
+        <FontAwesomeIcon icon={faBars} className="text-white text-2xl" />
+      </button>
     </header>
   );
 };
