46e79960f674e750bf7847bd933a85169e97d859
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-17 23:32:29 +0200

    added jest to the eslint configuration, now fixing tests

--- client/src/components/Header/tests/Header.test.js
+++ client/src/components/Header/tests/Header.test.js
@@ -1,9 +1,8 @@
 import { render } from '@testing-library/react';
-import {HeaderComponent} from '../Header';
+import { HeaderComponent } from '../Header';
 
 describe('It should properly render a Header component', () => {
-    test('should render Header component', () => {
-        render(<HeaderComponent />);
-     })
+  test('should render Header component', () => {
+    render(<HeaderComponent />);
+  });
 });
-
