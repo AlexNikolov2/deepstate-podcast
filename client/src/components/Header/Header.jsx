de9b48dab33ecd9f56c9da9a6e1c4bf5f5213264
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-19 20:53:55 +0200

    did the header, using the useState hook

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import image from '../../assets/img/Untitled.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { hamburger } from './utils/navButton';
+import { useState } from 'react';
 
 export const HeaderComponent = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
-    <header className="flex justify-around items-center gap-4" id="header">
+    <header
+      className={
+        'flex justify-around items-center gap-4' + (navbarOpen ? ' flex-col' : ' flex-row')
+      }
+      id="header">
       <div id="left" className="flex items-center gap-2">
         <img src={image} alt="" className="w-20" />
         <p className="text-4xl">Дълбоката държава</p>
       </div>
       <div className="flex gap-5 md:hidden">
-        <ul className="hidden md:flex gap-3" id="navbar">
+        <ul className={navbarOpen ? 'md:flex gap-3' : 'hidden'} id="navbar">
           <li>
             <a href="/" className="text-xl">
               Начало
@@ -28,7 +34,7 @@ export const HeaderComponent = () => {
             </a>
           </li>
         </ul>
-        <button onClick={hamburger} className="md:hidden">
+        <button onClick={() => setNavbarOpen(!navbarOpen)} className="md:hidden">
           <FontAwesomeIcon icon={faBars} className="text-white text-2xl" data-testid="icon" />
         </button>
       </div>
