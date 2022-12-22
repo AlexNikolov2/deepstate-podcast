745e746961e0bb5d525b02413af158ba150f6727
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-22 21:28:30 +0200

    added dynamic text for home item ,how on earth i've missed that

--- client/src/pages/home/components/HomeComponent.jsx
+++ client/src/pages/home/components/HomeComponent.jsx
@@ -6,9 +6,7 @@ export const HomeItem = ({ item }) => {
     <div className="item">
       <FontAwesomeIcon icon={item.icon} className="text-6xl text-sky-400" />
       <p className="item-title">{item.title}</p>
-      <p className="item-text">
-        В този сайт можете да намерите всички епизоди от подкаста &quot;Дълбоката държава&quot;.
-      </p>
+      <p className="item-text">{item.text}</p>
     </div>
   );
 };
