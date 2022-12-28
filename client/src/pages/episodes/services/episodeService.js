a90aac3fe6d3e411e51511c15424e491889b302a
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-29 01:52:21 +0200

    bitch, why we need separate service

--- client/src/pages/episodes/services/episodeService.js
+++ client/src/pages/episodes/services/episodeService.js
@@ -1,13 +1,1 @@
-export const getAllEpisodes = () => {
-  const link = 'https://api.spotify.com/v1/shows/4j4UH7lzr8UJhk3MvoCpEZ/episodes';
 
-  fetch(link, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + process.env.REACT_APP_KEY
-    }
-  })
-    .then((response) => response.json())
-    .then((responseData) => console.log(responseData));
-};
