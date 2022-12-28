21c5763a6d199abf4c2ddb3b2ec49a7e5aee1a8d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-12-29 00:23:23 +0200

    initalization of dumb sutff

--- client/src/pages/episodes/services/episodeService.js
+++ client/src/pages/episodes/services/episodeService.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
+
 export const getAllEpisodes = () => {
   const link = 'https://api.spotify.com/v1/shows/4j4UH7lzr8UJhk3MvoCpEZ/episodes';
 
-  fetch(link)
+  fetch(link, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + process.env.OAUTH_KEY
+    }
+  })
     .then((response) => response.json())
     .then((responseData) => console.log(responseData));
+  console.log(process.env.OAUTH_KEY);
 };
