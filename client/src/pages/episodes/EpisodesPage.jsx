6fdd29597cc3ef5953af678c01c0a37d31d0977b
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-02 18:03:57 +0200

    I am gonna try second approach for episodes

--- client/src/pages/episodes/EpisodesPage.jsx
+++ client/src/pages/episodes/EpisodesPage.jsx
@@ -1,34 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable space-before-blocks */
+/* eslint-disable space-before-function-paren */
 import { EpisodeItem } from './components/EpisodeComponent';
 import { useState, useEffect } from 'react';
 
 export const EpisodePage = () => {
   const [episodes, setEpisodes] = useState([]);
 
-  useEffect(() => {
+  /* useEffect(async () => {
     const arr = [];
 
     const link = 'https://api.spotify.com/v1/shows/4j4UH7lzr8UJhk3MvoCpEZ/episodes';
 
-    fetch(link, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + process.env.REACT_APP_KEY
-      }
-    })
-      .then((response) => response.json())
-      .then((responseData) => responseData.items.forEach((data) => arr.push(data)));
+    async function getEpisodes() {
+      const response = await fetch(link, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + process.env.REACT_APP_KEY
+        }
+      });
+
+      return response.json();
+    }
+
+    console.log(await getEpisodes());
 
     setEpisodes(arr);
-  }, []);
+  }, []); */
 
   return (
     <section>
       <p>Всички епизоди</p>
-      {episodes.map((item) => {
-        console.log(item);
-        return <EpisodeItem key={item.id} item={item} />;
-      })}
+      {<EpisodeItem />};
     </section>
   );
 };
