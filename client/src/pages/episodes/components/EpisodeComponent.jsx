0fae8291ff37d2eda898b107203311d117dd8aca
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-14 20:49:02 +0200

    skeleton for episode component

--- client/src/pages/episodes/components/EpisodeComponent.jsx
+++ client/src/pages/episodes/components/EpisodeComponent.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 
+import { format } from 'date-fns';
+
 export const EpisodeItem = ({ data }) => {
   return (
-    <section>
-      <p className="text-white">{data.title}</p>
+    <section className="flex flex-col gap-5 justify-center items-center content-center text-center">
+      <img src={data.itunes_image.href} alt="" className="w-32" />
+      <p>{data.title}</p>
+      <p>{data.description}</p>
+      <div id="info">
+        <p>{format(new Date(data.created), 'dd MMM yyyy')}</p>
+        <p>Времетраене: {data.itunes_duration}</p>
+      </div>
     </section>
   );
 };
