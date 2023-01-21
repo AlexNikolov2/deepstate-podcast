86d56c6f524e76219418e55a51ee297b652ced08
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-22 01:05:08 +0200

    guest page design first stage

--- client/src/pages/podcast-guests/components/GuestComponent.js
+++ client/src/pages/podcast-guests/components/GuestComponent.js
@@ -1,3 +1,11 @@
+import image from '../../assets/img/sasho-negara.jpg';
+
 export const GuestComponent = () => {
-  return <section></section>;
+  return (
+    <section id="guest" className="flex items-center justify-center gap-3 flex-col">
+      <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
+      <p className="text-4xl font-bold">Александър Стойчев</p>
+      <p className="text-xl text-slate-400">Е1: Какво е Дълбоката Държава?</p>
+    </section>
+  );
 };
