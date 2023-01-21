17a37cd203c985b58d7579149ea92bcfc530ceac
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-21 23:19:25 +0200

    fixec ids

--- client/src/data/guest-sample-data.js
+++ client/src/data/guest-sample-data.js
@@ -6,13 +6,13 @@ export const data = [
     episodes: ['Е1: Какво е Дълбоката Държава?']
   },
   {
-    id: 1,
+    id: 2,
     img: '../assets/img/sasho-negara.jpeg',
     name: 'Александър Стойчев',
     episodes: ['Е1: Какво е Дълбоката Държава?']
   },
   {
-    id: 1,
+    id: 3,
     img: '../assets/img/sasho-negara.jpeg',
     name: 'Александър Стойчев',
     episodes: ['Е1: Какво е Дълбоката Държава?']
