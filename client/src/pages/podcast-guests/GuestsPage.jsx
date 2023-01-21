fa23a4519e17114515e29960d1ccedf2ecaae136
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-22 01:20:01 +0200

    text and style fixes for guest page

--- client/src/pages/podcast-guests/GuestsPage.jsx
+++ client/src/pages/podcast-guests/GuestsPage.jsx
@@ -2,26 +2,28 @@ import image from '../../assets/img/sasho-negara.jpg';
 
 export const GuestPage = () => {
   return (
-    <section id="guest-page">
-      <p>Гостите на подкаста</p>
-      <p>Това са всички хора, които са били гости в подкаста &quot;Дълбоката държава&quot;</p>
+    <section id="guest-page" className="flex flex-col gap-6 justify-center items-center py-5">
+      <p className="text-5xl font-bold text-center">Гостите на подкаста</p>
+      <p className="text-xl text-center">
+        Това са всички хора, които са били гости в подкаста &quot;Дълбоката държава&quot;
+      </p>
       <div
         id="guest-items"
         className="flex flex-col md:flex-row gap-16 justify-center items-center">
         <div id="guest" className="flex items-center justify-center gap-3 flex-col">
           <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
           <p className="text-[30px] md:text-4xl font-bold text-center">Александър Стойчев</p>
-          <p className="text-xl text-slate-400">Е1: Какво е Дълбоката Държава?</p>
+          <p className="text-xl text-slate-400 text-center">Е1: Какво е Дълбоката Държава?</p>
         </div>
         <div id="guest" className="flex items-center justify-center gap-3 flex-col">
           <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
-          <p className="text-4xl font-bold">Александър Стойчев</p>
-          <p className="text-xl text-slate-400">Е1: Какво е Дълбоката Държава?</p>
+          <p className="text-[30px] md:text-4xl font-bold text-center">Александър Стойчев</p>
+          <p className="text-xl text-slate-400 text-center">Е1: Какво е Дълбоката Държава?</p>
         </div>
         <div id="guest" className="flex items-center justify-center gap-3 flex-col">
           <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
-          <p className="text-4xl font-bold">Александър Стойчев</p>
-          <p className="text-xl text-slate-400">Е1: Какво е Дълбоката Държава?</p>
+          <p className="text-[30px] md:text-4xl font-bold text-center">Александър Стойчев</p>
+          <p className="text-xl text-slate-400 text-center">Е1: Какво е Дълбоката Държава?</p>
         </div>
       </div>
     </section>
