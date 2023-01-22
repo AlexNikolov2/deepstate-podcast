561347989aaaf8545363d73ab4339e747e5cc590
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-23 00:15:13 +0200

    small footer fix

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -17,31 +17,12 @@ export const HeaderComponent = () => {
       <div id="left" className="flex items-center gap-2">
         <img src={image} alt="" className="w-20" />
         <p className="text-4xl">Дълбоката държава</p>
+        <button onClick={() => setNavbarOpen(!navbarOpen)} className="md:hidden relative right-5">
+          <FontAwesomeIcon icon={faBars} className="text-white text-4xl" data-testid="icon" />
+        </button>
       </div>
       <div className="flex gap-5 md:hidden">
         <ul className={navbarOpen ? 'md:flex gap-3' : 'hidden'} id="navbar">
-          <li>
-            <Link to="/" className="text-xl">
-              Начало
-            </Link>
-          </li>
-          <li>
-            <Link to="/episodes" className="text-xl">
-              Епизоди
-            </Link>
-          </li>
-          <li>
-            <Link to="/guests" className="text-xl">
-              Гости
-            </Link>
-          </li>
-        </ul>
-        <button onClick={() => setNavbarOpen(!navbarOpen)} className="md:hidden">
-          <FontAwesomeIcon icon={faBars} className="text-white text-2xl" data-testid="icon" />
-        </button>
-      </div>
-      <div className="md:flex md:gap-5">
-        <ul className="hidden md:flex gap-3" id="navbar">
           <li>
             <Link to="/" className="text-xl">
               Начало
