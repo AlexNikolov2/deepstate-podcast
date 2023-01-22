a153edf6208a904df36e4f2ac9b81dbf9f20822e
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-22 23:45:45 +0200

    WHAT THE HELL

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,3 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { brands } from '@fortawesome/fontawesome-svg-core';
+
 export const FooterComponent = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div id="left">
+        <p>Дълбоката държава</p>
+        <p>&copy;2023. All rights reserved.</p>
+      </div>
+      <div id="right">
+        <FontAwesomeIcon icon={brands('facebook')} className="text-white" />
+      </div>
+    </footer>
+  );
 };
