57e1ec735b70627b3b261469132076798ac16554
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-22 22:30:25 +0200

    full data

--- client/src/data/guest-sample-data.js
+++ client/src/data/guest-sample-data.js
@@ -7,14 +7,14 @@ export const data = [
   },
   {
     id: 2,
-    img: '../assets/img/richard.png',
-    name: 'Александър Стойчев',
-    episodes: ['Е1: Какво е Дълбоката Държава?']
+    img: '../assets/img/no2.png',
+    name: 'Антонио Коларов',
+    episodes: ['Е2: Изборът пред нас']
   },
   {
     id: 3,
-    img: '../assets/img/sasho-negara.jpg',
-    name: 'Александър Стойчев',
-    episodes: ['Е1: Какво е Дълбоката Държава?']
+    img: '../assets/img/no3.png',
+    name: 'Борис Гичев',
+    episodes: ['Е3: Технологичното робство']
   }
 ];
