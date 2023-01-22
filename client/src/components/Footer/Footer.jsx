dbbde85fff49151a36cb281ef5ebde9f8787c3e5
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-23 00:08:39 +0200

    footer fixed and done!

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { faFacebook, faInstagram, faSpotify } from '@fortawesome/free-brands-svg
 
 export const FooterComponent = () => {
   return (
-    <footer className="flex flex-col md:flex-row justify-between p-5 bottom-0 md:text-center gap-5">
+    <footer className="flex flex-col md:flex-row justify-between p-5 bottom-0 md_rev:text-center gap-5">
       <div id="left" className="flex flex-col gap-5">
         <p className="text-5xl font-bold">Дълбоката държава</p>
         <p className="text-[24px]">&copy;2023. All rights reserved.</p>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     screens: {
       md: '769px',
-      tablet: { min: '1350px' }
+      tablet: { min: '1350px' },
+      md_rev: { max: '768px' }
     },
     extend: {}
   },
