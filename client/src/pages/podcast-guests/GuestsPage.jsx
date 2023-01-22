a264e37f8bc3d057e1c4b044a0a27f8d7b9e0c48
Aleks Nikolov <tcr.official.world@gmail.com>
2023-01-22 02:03:18 +0200

    guest page well designed, i think

--- client/src/pages/podcast-guests/GuestsPage.jsx
+++ client/src/pages/podcast-guests/GuestsPage.jsx
@@ -2,7 +2,9 @@ import image from '../../assets/img/sasho-negara.jpg';
 
 export const GuestPage = () => {
   return (
-    <section id="guest-page" className="flex flex-col gap-6 justify-center items-center py-5">
+    <section
+      id="guest-page"
+      className="flex flex-col gap-6 justify-center items-center py-5 episode-header">
       <p className="text-5xl font-bold text-center">Гостите на подкаста</p>
       <p className="text-xl text-center">
         Това са всички хора, които са били гости в подкаста &quot;Дълбоката държава&quot;
@@ -13,17 +15,17 @@ export const GuestPage = () => {
         <div id="guest" className="flex items-center justify-center gap-3 flex-col">
           <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
           <p className="text-[30px] md:text-4xl font-bold text-center">Александър Стойчев</p>
-          <p className="text-xl text-slate-400 text-center">Е1: Какво е Дълбоката Държава?</p>
+          <p className="text-xl text-center">Е1: Какво е Дълбоката Държава?</p>
         </div>
         <div id="guest" className="flex items-center justify-center gap-3 flex-col">
           <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
           <p className="text-[30px] md:text-4xl font-bold text-center">Александър Стойчев</p>
-          <p className="text-xl text-slate-400 text-center">Е1: Какво е Дълбоката Държава?</p>
+          <p className="text-xl text-center">Е1: Какво е Дълбоката Държава?</p>
         </div>
         <div id="guest" className="flex items-center justify-center gap-3 flex-col">
           <img src={image} alt="" className="rounded-full w-[256px] border-[14px]" />
           <p className="text-[30px] md:text-4xl font-bold text-center">Александър Стойчев</p>
-          <p className="text-xl text-slate-400 text-center">Е1: Какво е Дълбоката Държава?</p>
+          <p className="text-xl text-center">Е1: Какво е Дълбоката Държава?</p>
         </div>
       </div>
     </section>
