88ca3e31f16cdcdf8200335bd172034aeca5ab27
Aleks Nikolov <tcr.official.world@gmail.com>
2023-02-03 22:12:53 +0200

    fixed guest page

--- client/src/pages/podcast-guests/GuestsPage.jsx
+++ client/src/pages/podcast-guests/GuestsPage.jsx
@@ -17,14 +17,14 @@ export const GuestPage = () => {
   return (
     <section
       id="guest-page"
-      className="flex flex-col gap-6 justify-center items-center py-5 episode-header">
+      className="flex flex-col gap-6 justify-center items-center py-5 episode-header ">
       <p className="text-5xl font-bold text-center">Гостите на подкаста</p>
       <p className="text-xl text-center">
         Това са всички хора, които са били гости в подкаста &quot;Дълбоката държава&quot;.
       </p>
       <div
         id="guest-items"
-        className="flex flex-col md:flex-row gap-16 justify-center items-center">
+        className="flex flex-col md:flex-row gap-16 justify-center items-center flex-wrap">
         {items.map((item) => {
           return <GuestComponent key={item.id} item={item} />;
         })}
--- client/src/pages/podcast-guests/components/GuestComponent.js
+++ client/src/pages/podcast-guests/components/GuestComponent.js
@@ -2,10 +2,12 @@
 
 export const GuestComponent = ({ item }) => {
   return (
-    <section id="guest" className="flex items-center justify-center gap-3 flex-col">
+    <section
+      id="guest"
+      className="flex items-center justify-center gap-3 flex-col max-w-[365px] min-w-[300px]">
       <img src={item.img} alt="" className="rounded-full w-[256px] border-[14px]" />
       <p className="text-4xl font-bold text-center">{item.name}</p>
-      <p className="text-xl font-semibold">{item.episodes}</p>
+      <p className="text-xl font-semibold text-center">{item.episodes}</p>
     </section>
   );
 };
